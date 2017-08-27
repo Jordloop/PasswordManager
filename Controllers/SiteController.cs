2885520161ce5e8459a7f322e4246ddf5f2b7232
JordLoop <Jordloop@gmail.com>
2017-08-26 23:05:52 -0700

    update SiteController.cs class name to fix bug. SiteController.cs full CRUD functionality confirmed

--- Controllers/SiteController.cs
+++ Controllers/SiteController.cs
@@ -9,69 +9,66 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PasswordManager.Controllers
 {
-    public class SiteController
+    [Authorize]
+    public class SiteController : Controller
     {
-        [Authorize]
-        public class ToDoController : Controller
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SiteController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
-            private readonly ApplicationDbContext _db;
-            private readonly UserManager<ApplicationUser> _userManager;
-
-            public ToDoController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
-            {
-                _userManager = userManager;
-                _db = db;
-            }
+            _userManager = userManager;
+            _db = db;
+        }
 //Index - Returns all of authenticated user's Site objects
-            public async Task<IActionResult> Index()
-            {
-                var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-                var currentUser = await _userManager.FindByIdAsync(userId);
-                return View(_db.Sites.Where(x => x.User.Id == currentUser.Id));
-            }
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            return View(_db.Sites.Where(x => x.User.Id == currentUser.Id));
+        }
 //Create
-            public IActionResult Create()
-            {
-                return View();
-            }
-            [HttpPost]
-            public async Task<IActionResult> Create(Site site)
-            {
-                var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-                var currentUser = await _userManager.FindByIdAsync(userId);
-                site.User = currentUser;
-                _db.Sites.Add(site);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Site site)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            site.User = currentUser;
+            _db.Sites.Add(site);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 //Edit
-            public IActionResult Edit(int id)
-            {
-                var thisSite= _db.Sites.FirstOrDefault(sites => sites.Id == id);
-                return View(thisSite);
-            }
-            [HttpPost]
-            public IActionResult Edit(Site site)
-            {
-                _db.Entry(site).State = EntityState.Modified;
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        public IActionResult Edit(int id)
+        {
+            var thisSite= _db.Sites.FirstOrDefault(sites => sites.Id == id);
+            return View(thisSite);
+        }
+        [HttpPost]
+        public IActionResult Edit(Site site)
+        {
+            _db.Entry(site).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 //Delete
-            public IActionResult Delete(int id)
-            {
-                var thisSite = _db.Sites.FirstOrDefault(sites => sites.Id == id);
-                return View(thisSite);
-            }
+        public IActionResult Delete(int id)
+        {
+            var thisSite = _db.Sites.FirstOrDefault(sites => sites.Id == id);
+            return View(thisSite);
+        }
 
-            [HttpPost, ActionName("Delete")]
-            public IActionResult DeleteConfirmed(int id)
-            {
-                var thisItem = _db.Sites.FirstOrDefault(sites => sites.Id == id);
-                _db.Sites.Remove(thisItem);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = _db.Sites.FirstOrDefault(sites => sites.Id == id);
+            _db.Sites.Remove(thisItem);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
