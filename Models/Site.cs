5b47b0dfb6219453670ed037f2b45ae08237e7e8
JordLoop <Jordloop@gmail.com>
2017-08-26 20:14:31 -0700

    update Site.cs to include an Id property

--- Models/Site.cs
+++ Models/Site.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PasswordManager.Models
 {
     [Table("Sites")]
     public class Site
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
         public string UserName { get; set; }
