cb5849d8636d48e22e0118e72e455cc1072f0b3e
JordLoop <Jordloop@gmail.com>
2017-08-26 20:12:30 -0700

    add Site object model

---
+++ Models/Site.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PasswordManager.Models
+{
+    [Table("Sites")]
+    public class Site
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
