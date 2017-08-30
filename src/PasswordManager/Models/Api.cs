e982fba20d39cedd9edda214bc373129c846bdbd
Jordan Loop <Jordloop@gmail.com>
2017-08-30 15:06:24 -0700

    add Models/Api.cs

---
+++ src/PasswordManager/Models/Api.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace PasswordManager.Models
+{
+    [Table("Apis")]
+    public class Api
+    {
+        [Key]
+        public int Id { get;set; }
+        public string Name { get;set; }
+        public string Key { get;set; }
+        public string Documentation { get;set; }
+
+    }
+}
