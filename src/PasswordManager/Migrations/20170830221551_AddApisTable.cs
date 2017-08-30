de3a98038fc4e8da66c1476f4594c902242ae58a
Jordan Loop <Jordloop@gmail.com>
2017-08-30 15:17:02 -0700

    update Models/Api.cs and update database migrations

---
+++ src/PasswordManager/Migrations/20170830221551_AddApisTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PasswordManager.Migrations
+{
+    public partial class AddApisTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- src/PasswordManager/Models/Api.cs
+++ src/PasswordManager/Models/Api.cs
@@ -13,7 +13,7 @@ namespace PasswordManager.Models
         public int Id { get;set; }
         public string Name { get;set; }
         public string Key { get;set; }
-        public string Documentation { get;set; }
+        public string Link { get;set; }
 
     }
 }
